cb32a697929f0154102e019350bb7de7d835563c
lyluck <lyluck@pixiv.co.jp>
2026-07-13 13:18:30 +0900

    Manager contextの前準備をSynchronizedBeforeSuiteに引っ越し

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -20,12 +20,25 @@ import (
 	"fmt"
 	"os/exec"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/pixiv/k8s-job-wrapper/test/utils"
 )
+
+// namespace where the project is deployed in
+const namespace = "k8s-job-wrapper-system"
+
+// serviceAccountName created for the project
+const serviceAccountName = "k8s-job-wrapper-controller-manager"
+
+// metricsServiceName is the name of the metrics service of the project
+const metricsServiceName = "k8s-job-wrapper-controller-manager-metrics-service"
+
+// metricsRoleBindingName is the name of the RBAC that will be created to allow get the metrics data
+const metricsRoleBindingName = "k8s-job-wrapper-metrics-binding"
 
 var (
 	// Optional Environment Variables:
@@ -35,6 +48,8 @@ var (
 	skipCertManagerInstall = utils.IsEnvTrue("CERT_MANAGER_INSTALL_SKIP")
 	// isCertManagerAlreadyInstalled will be set true when CertManager CRDs be found on the cluster
 	isCertManagerAlreadyInstalled = false
+
+	controllerPodName string // should be initialized by SynchronizedBeforeSuite.
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
@@ -109,6 +124,131 @@ var _ = SynchronizedBeforeSuite(func() {
 		_, err = utils.Run(cmd)
 		Expect(err).NotTo(HaveOccurred(), "Failed to deploy the controller-manager")
 	}
+
+	// The timeout duration for Eventually().
+	// If it takes longer than this, the test will be treated as a failure.
+	// Therefore, the verification job is designed to be quick to execute.
+	// The container image is also pre-pulled.
+	// The cronjob is set to trigger every minute.
+	// It's best to split Eventually() calls and reduce the number of operations to wait for within a single call.
+	SetDefaultEventuallyTimeout(2 * time.Minute)
+	SetDefaultEventuallyPollingInterval(time.Second)
+
+	By("validating that the controller-manager pod is running as expected")
+	verifyControllerUp := func(g Gomega) {
+		// Get the name of the controller-manager pod
+		cmd := utils.KubectlCmd("get",
+			"pods", "-l", "control-plane=controller-manager",
+			"-o", "go-template={{ range .items }}"+
+				"{{ if not .metadata.deletionTimestamp }}"+
+				"{{ .metadata.name }}"+
+				"{{ \"\\n\" }}{{ end }}{{ end }}",
+			"-n", namespace,
+		)
+
+		podOutput, err := utils.Run(cmd)
+		g.Expect(err).NotTo(HaveOccurred(), "Failed to retrieve controller-manager pod information")
+		podNames := utils.GetNonEmptyLines(podOutput)
+		g.Expect(podNames).To(HaveLen(1), "expected 1 controller pod running")
+		controllerPodName = podNames[0]
+		g.Expect(controllerPodName).To(ContainSubstring("controller-manager"))
+
+		// Validate the pod's status
+		cmd = utils.KubectlCmd("get",
+			"pods", controllerPodName, "-o", "jsonpath={.status.phase}",
+			"-n", namespace,
+		)
+		output, err := utils.Run(cmd)
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(output).To(Equal("Running"), "Incorrect controller-manager pod status")
+	}
+	Eventually(verifyControllerUp).Should(Succeed())
+
+	By("creating a ClusterRoleBinding for the service account to allow access to metrics")
+	cmd = utils.KubectlCmd("create", "clusterrolebinding", metricsRoleBindingName,
+		"--clusterrole=k8s-job-wrapper-metrics-reader",
+		fmt.Sprintf("--serviceaccount=%s:%s", namespace, serviceAccountName),
+	)
+	_, err = utils.Run(cmd)
+	Expect(err).NotTo(HaveOccurred(), "Failed to create ClusterRoleBinding")
+
+	By("validating that the metrics service is available")
+	cmd = utils.KubectlCmd("get", "service", metricsServiceName, "-n", namespace)
+	_, err = utils.Run(cmd)
+	Expect(err).NotTo(HaveOccurred(), "Metrics service should exist")
+
+	By("getting the service account token")
+	token, err := serviceAccountToken()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(token).NotTo(BeEmpty())
+
+	By("waiting for the metrics endpoint to be ready")
+	verifyMetricsEndpointReady := func(g Gomega) {
+		cmd := utils.KubectlCmd("get", "endpoints", metricsServiceName, "-n", namespace)
+		output, err := utils.Run(cmd)
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(output).To(ContainSubstring("8443"), "Metrics endpoint is not ready")
+	}
+	Eventually(verifyMetricsEndpointReady).Should(Succeed())
+
+	By("verifying that the controller manager is serving the metrics server")
+	verifyMetricsServerStarted := func(g Gomega) {
+		cmd := utils.KubectlCmd("logs", controllerPodName, "-n", namespace)
+		output, err := utils.Run(cmd)
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(output).To(ContainSubstring("controller-runtime.metrics\tServing metrics server"),
+			"Metrics server not yet started")
+	}
+	Eventually(verifyMetricsServerStarted).Should(Succeed())
+
+	By("creating the curl-metrics pod to access the metrics endpoint")
+	cmd = utils.KubectlCmd("run", "curl-metrics", "--restart=Never",
+		"--namespace", namespace,
+		"--image=curlimages/curl:latest",
+		"--overrides",
+		fmt.Sprintf(`{
+					"spec": {
+						"containers": [{
+							"name": "curl",
+							"image": "curlimages/curl:latest",
+							"command": ["/bin/sh", "-c"],
+							"args": ["curl -v -k -H 'Authorization: Bearer %s' https://%s.%s.svc.cluster.local:8443/metrics"],
+							"securityContext": {
+								"allowPrivilegeEscalation": false,
+								"capabilities": {
+									"drop": ["ALL"]
+								},
+								"runAsNonRoot": true,
+								"runAsUser": 1000,
+								"seccompProfile": {
+									"type": "RuntimeDefault"
+								}
+							}
+						}],
+						"serviceAccount": "%s"
+					}
+				}`, token, metricsServiceName, namespace, serviceAccountName))
+	_, err = utils.Run(cmd)
+	Expect(err).NotTo(HaveOccurred(), "Failed to create curl-metrics pod")
+
+	By("waiting for the curl-metrics pod to complete.")
+	verifyCurlUp := func(g Gomega) {
+		cmd := utils.KubectlCmd("get", "pods", "curl-metrics",
+			"-o", "jsonpath={.status.phase}",
+			"-n", namespace)
+		output, err := utils.Run(cmd)
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(output).To(Equal("Succeeded"), "curl pod in wrong status")
+	}
+	Eventually(verifyCurlUp, 5*time.Minute).Should(Succeed())
+
+	By("getting the metrics by checking curl-metrics logs")
+	metricsOutput := getMetricsOutput()
+	Expect(metricsOutput).To(ContainSubstring(
+		"controller_runtime_reconcile_total",
+	))
+
+	// +kubebuilder:scaffold:e2e-webhooks-checks
 }, func() {})
 
 var _ = SynchronizedAfterSuite(func() {}, func() {
--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -23,7 +23,6 @@ import (
 	"path/filepath"
 	"slices"
 	"strings"
-	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -34,21 +33,7 @@ import (
 	"github.com/pixiv/k8s-job-wrapper/test/utils"
 )
 
-// namespace where the project is deployed in
-const namespace = "k8s-job-wrapper-system"
-
-// serviceAccountName created for the project
-const serviceAccountName = "k8s-job-wrapper-controller-manager"
-
-// metricsServiceName is the name of the metrics service of the project
-const metricsServiceName = "k8s-job-wrapper-controller-manager-metrics-service"
-
-// metricsRoleBindingName is the name of the RBAC that will be created to allow get the metrics data
-const metricsRoleBindingName = "k8s-job-wrapper-metrics-binding"
-
 var _ = Describe("Manager", Ordered, func() {
-	var controllerPodName string
-
 	// After each test, check for failures and collect logs, events,
 	// and pod descriptions for debugging.
 	AfterEach(func() {
@@ -92,135 +77,7 @@ var _ = Describe("Manager", Ordered, func() {
 		}
 	})
 
-	// The timeout duration for Eventually().
-	// If it takes longer than this, the test will be treated as a failure.
-	// Therefore, the verification job is designed to be quick to execute.
-	// The container image is also pre-pulled.
-	// The cronjob is set to trigger every minute.
-	// It's best to split Eventually() calls and reduce the number of operations to wait for within a single call.
-	SetDefaultEventuallyTimeout(2 * time.Minute)
-	SetDefaultEventuallyPollingInterval(time.Second)
-
 	Context("Manager", func() {
-		It("should run successfully", func() {
-			By("validating that the controller-manager pod is running as expected")
-			verifyControllerUp := func(g Gomega) {
-				// Get the name of the controller-manager pod
-				cmd := utils.KubectlCmd("get",
-					"pods", "-l", "control-plane=controller-manager",
-					"-o", "go-template={{ range .items }}"+
-						"{{ if not .metadata.deletionTimestamp }}"+
-						"{{ .metadata.name }}"+
-						"{{ \"\\n\" }}{{ end }}{{ end }}",
-					"-n", namespace,
-				)
-
-				podOutput, err := utils.Run(cmd)
-				g.Expect(err).NotTo(HaveOccurred(), "Failed to retrieve controller-manager pod information")
-				podNames := utils.GetNonEmptyLines(podOutput)
-				g.Expect(podNames).To(HaveLen(1), "expected 1 controller pod running")
-				controllerPodName = podNames[0]
-				g.Expect(controllerPodName).To(ContainSubstring("controller-manager"))
-
-				// Validate the pod's status
-				cmd = utils.KubectlCmd("get",
-					"pods", controllerPodName, "-o", "jsonpath={.status.phase}",
-					"-n", namespace,
-				)
-				output, err := utils.Run(cmd)
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(output).To(Equal("Running"), "Incorrect controller-manager pod status")
-			}
-			Eventually(verifyControllerUp).Should(Succeed())
-		})
-
-		It("should ensure the metrics endpoint is serving metrics", func() {
-			By("creating a ClusterRoleBinding for the service account to allow access to metrics")
-			cmd := utils.KubectlCmd("create", "clusterrolebinding", metricsRoleBindingName,
-				"--clusterrole=k8s-job-wrapper-metrics-reader",
-				fmt.Sprintf("--serviceaccount=%s:%s", namespace, serviceAccountName),
-			)
-			_, err := utils.Run(cmd)
-			Expect(err).NotTo(HaveOccurred(), "Failed to create ClusterRoleBinding")
-
-			By("validating that the metrics service is available")
-			cmd = utils.KubectlCmd("get", "service", metricsServiceName, "-n", namespace)
-			_, err = utils.Run(cmd)
-			Expect(err).NotTo(HaveOccurred(), "Metrics service should exist")
-
-			By("getting the service account token")
-			token, err := serviceAccountToken()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(token).NotTo(BeEmpty())
-
-			By("waiting for the metrics endpoint to be ready")
-			verifyMetricsEndpointReady := func(g Gomega) {
-				cmd := utils.KubectlCmd("get", "endpoints", metricsServiceName, "-n", namespace)
-				output, err := utils.Run(cmd)
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(output).To(ContainSubstring("8443"), "Metrics endpoint is not ready")
-			}
-			Eventually(verifyMetricsEndpointReady).Should(Succeed())
-
-			By("verifying that the controller manager is serving the metrics server")
-			verifyMetricsServerStarted := func(g Gomega) {
-				cmd := utils.KubectlCmd("logs", controllerPodName, "-n", namespace)
-				output, err := utils.Run(cmd)
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(output).To(ContainSubstring("controller-runtime.metrics\tServing metrics server"),
-					"Metrics server not yet started")
-			}
-			Eventually(verifyMetricsServerStarted).Should(Succeed())
-
-			By("creating the curl-metrics pod to access the metrics endpoint")
-			cmd = utils.KubectlCmd("run", "curl-metrics", "--restart=Never",
-				"--namespace", namespace,
-				"--image=curlimages/curl:latest",
-				"--overrides",
-				fmt.Sprintf(`{
-					"spec": {
-						"containers": [{
-							"name": "curl",
-							"image": "curlimages/curl:latest",
-							"command": ["/bin/sh", "-c"],
-							"args": ["curl -v -k -H 'Authorization: Bearer %s' https://%s.%s.svc.cluster.local:8443/metrics"],
-							"securityContext": {
-								"allowPrivilegeEscalation": false,
-								"capabilities": {
-									"drop": ["ALL"]
-								},
-								"runAsNonRoot": true,
-								"runAsUser": 1000,
-								"seccompProfile": {
-									"type": "RuntimeDefault"
-								}
-							}
-						}],
-						"serviceAccount": "%s"
-					}
-				}`, token, metricsServiceName, namespace, serviceAccountName))
-			_, err = utils.Run(cmd)
-			Expect(err).NotTo(HaveOccurred(), "Failed to create curl-metrics pod")
-
-			By("waiting for the curl-metrics pod to complete.")
-			verifyCurlUp := func(g Gomega) {
-				cmd := utils.KubectlCmd("get", "pods", "curl-metrics",
-					"-o", "jsonpath={.status.phase}",
-					"-n", namespace)
-				output, err := utils.Run(cmd)
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(output).To(Equal("Succeeded"), "curl pod in wrong status")
-			}
-			Eventually(verifyCurlUp, 5*time.Minute).Should(Succeed())
-
-			By("getting the metrics by checking curl-metrics logs")
-			metricsOutput := getMetricsOutput()
-			Expect(metricsOutput).To(ContainSubstring(
-				"controller_runtime_reconcile_total",
-			))
-		})
-
-		// +kubebuilder:scaffold:e2e-webhooks-checks
 
 		// TODO: Customize the e2e test suite with scenarios specific to your project.
 		// Consider applying sample/CR(s) and check their status and/or verifying
